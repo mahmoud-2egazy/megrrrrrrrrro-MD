f31ec7a5004ee00a3578325b70173b51df136b81
mahmoud-2egazy <mahmoud.tarboo@gmail.com>
2024-07-23 03:09:01 +0300

    Delete plugins directory

--- plugins/owner-unbanchat.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = false
-m.reply('*[🌐] هذه الدردشه غير محظوره *')
-}
-handler.help = ['unbanchat']
-handler.tags = ['owner']
-handler.command = /^بان_شات_فك$/i
-export default handler
